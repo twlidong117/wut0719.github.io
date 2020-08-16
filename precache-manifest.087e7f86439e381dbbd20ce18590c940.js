a3dcd3a53edd84c7a3186ce8dd589573607090fc
wut0719 <wut0719@gmail.com>
2020-08-16 18:16:15 +0800

    Updates

---
+++ precache-manifest.087e7f86439e381dbbd20ce18590c940.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea7e854139c0654ce826d1c285e71899",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7614461c932f1a9cb97",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "94265a0299f9c68272c8",
+    "url": "/static/js/2.9633bfa6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9633bfa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7614461c932f1a9cb97",
+    "url": "/static/js/main.6bf7100a.chunk.js"
+  },
+  {
+    "revision": "98a7bf8489210e95df7e",
+    "url": "/static/js/runtime-main.833bd714.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
